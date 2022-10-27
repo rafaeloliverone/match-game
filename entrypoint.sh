6aa84209c58da71142f37192704f7fc2ae5671e6
Rafael Oliveira <rafaelbatistacg@gmail.com>
2022-10-27 01:41:33 -0300

    running files of migrations and file shell to collect statics

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+python manage.py migrate
+python manage.py collectstatic --no-input
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+python manage.py createsuperuser \
+    --noinput \
+    --username $DJANGO_SUPERUSER_USERNAME \
+    --email $DJANGO_SUPERUSER_EMAIL
+fi
+$@
+
+python manage.py runserver 0.0.0.0:8000
+# python manage.py loaddata ./suppliers/database/loaddata.json
+
+# gunicorn game.wsgi:application --bind 0.0.0.0:8000
---
+++ match/utils/models.py
@@ -0,0 +1,6 @@
+MAP_CHOICES = (
+    ("Dust2", "Dust2"),
+    ("Overpass", "Overpass"),
+    ("Train", "Train"),
+    ("Mirage", "Mirage"),
+)
